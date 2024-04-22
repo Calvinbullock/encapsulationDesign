14db851f4f54c9273ed591e184048df63290d580
Calvinbullock <calvinnbullock@gmail.com>
2024-04-22 09:21:21 -0600

    helloworld update

--- 230.01.Ponder/lab01.cpp
+++ 230.01.Ponder/lab01.cpp
@@ -20,6 +20,6 @@ using namespace std;
  **********************************************************/
 int main() {
     // print hello world to console
-    cout << "hello World" << endl;
+    cout << "Hello World" << endl;
     return 0;
 }
