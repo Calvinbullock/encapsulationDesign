56e57391ecd638b240b25d3bd437907f6d9e2c25
Calvinbullock <calvinnbullock@gmail.com>
2024-04-22 09:16:30 -0600

    first commit

---
+++ 230.01.Ponder/lab01.cpp
@@ -0,0 +1,26 @@
+/*************************************************************
+ * 1. Name:
+ *      Calvin Bullock
+ * 2. Assignment Name:
+ *      Lab 01: Hello World
+ * 3. Assignment Description:
+ *      Please the text "Hello world" on the screen
+ * 4. What was the hardest part? Be as specific as possible.
+ *      ??
+ * 5. How long did it take for you to complete the assignment?
+ *      ??
+ *****************************************************************/
+
+#include <iostream>
+
+using namespace std; 
+
+/***********************************************************
+ * Print out hello world
+ **********************************************************/
+int main() {
+    // print hello world to console
+    cout << "hello World" << endl;
+    return 0;
+}
+
