3ecb24683b6616d15461e7a39f52eb5200a532ce
Calvinbullock <calvinnbullock@gmail.com>
2024-04-29 08:11:55 -0600

    week2 start

---
+++ week2/lab02.cpp
@@ -0,0 +1,27 @@
+
+
+
+/*
+What is your horizontal velocity (m/s)? 0
+What is your vertical velocity (m/s)? -10
+What is your altitude (m)? 100
+What is the angle of the LM where 0 is up (degrees)? 60
+
+For the next 5 seconds with the main engine on, the position of the lander is:
+
+ 1s - x,y:(3.87, 89.80)m  dx,dy:(2.58, -10.14)m/s  speed:10.46m/s  angle:60.00deg
+ 2s - x,y:(10.32, 79.46)m  dx,dy:(5.16, -10.27)m/s  speed:11.49m/s  angle:60.00deg
+ 3s - x,y:(19.35, 68.99)m  dx,dy:(7.74, -10.41)m/s  speed:12.97m/s  angle:60.00deg
+ 4s - x,y:(30.96, 58.38)m  dx,dy:(10.32, -10.54)m/s  speed:14.75m/s  angle:60.00deg
+ 5s - x,y:(45.16, 47.63)m  dx,dy:(12.90, -10.68)m/s  speed:16.75m/s  angle:60.00deg
+
+What is the new angle of the LM where 0 is up (degrees)? -60
+
+For the next 5 seconds with the main engine on, the position of the lander is:
+
+ 6s - x,y:(54.19, 36.75)m  dx,dy:(10.32, -10.81)m/s  speed:14.95m/s  angle:-60.00deg
+ 7s - x,y:(60.64, 25.74)m  dx,dy:(7.74, -10.95)m/s  speed:13.41m/s  angle:-60.00deg
+ 8s - x,y:(64.51, 14.59)m  dx,dy:(5.16, -11.08)m/s  speed:12.22m/s  angle:-60.00deg
+ 9s - x,y:(65.80, 3.31)m  dx,dy:(2.58, -11.22)m/s  speed:11.51m/s  angle:-60.00deg
+10s - x,y:(64.51, -8.11)m  dx,dy:(0.00, -11.35)m/s  speed:11.35m/s  angle:-60.00deg
+*/
