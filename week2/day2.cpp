ea36aedb1f6b0095f8a25579ad6be3f9b24c4b53
Calvinbullock <calvinnbullock@gmail.com>
2024-05-02 10:44:51 -0600

    new files week2

---
+++ week2/day2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int array[4];
+   array[0] = 1;
+   array[1] = 2;
+   array[2] = 3;
+   array[3] = 4;
+
+   for (int i = 0; i < 4; i++) 
+   {
+      cout << array[i] << endl;
+   }
+}
---
+++ week2/lab2.cpp
@@ -0,0 +1,12 @@
+/*************************************************************
+ * 1. Name:
+ *      Calvin Bullock, Hyrum Bullock
+ * 2. Assignment Name:
+ *      Lab 02: Apollo 11
+ * 3. Assignment Description:
+ *      Simulate the Apollo 11 landing
+ * 4. What was the hardest part? Be as specific as possible.
+ *      TODO -a paragraph or two about how the assignment went for you-
+ * 5. How long did it take for you to complete the assignment?
+ *      TODO -total time in hours: reading the assignment, submitting, and writing code-
+ *****************************************************************/
