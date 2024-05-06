c96632c4ce151fd7095b71245cd94d0bad4fa2db
HyrumB <hyrumbullock@gmail.com>
2024-05-06 10:03:28 -0600

    started working on angle class

--- week3/angle.cpp
+++ week3/angle.cpp
@@ -8,17 +8,63 @@
  ************************************************************************/
 
 #include "angle.h"
-#include <math.h>  // for floor()
+#include <math.h> // for floor()
 #include <cassert>
 using namespace std;
 
- /************************************
-  * ANGLE : NORMALIZE
-  ************************************/
-double Angle::normalize(double radians) const
+class Angle
 {
-   return -99.9;
-}
+private:
+   double radian;
 
+public:
+   // Angle() {}
+   // + Angle(degrees : Double)
+   // + Angle(rhs : Angle)
+   double getDegrees()
+   {
+      return convertToDegrees(radian);
+   }
 
+   double getRadians()
+   {
+      return radian;
+   }
 
+   double setDegrees(double new_degree)
+   {
+      radian = convertToRadians(new_degree);
+   }
+
+   double setRadians(double new_radian)
+   {
+      radian = new_radian;
+   }
+   // + setUp()
+   // + setDown()
+   // + setLeft()
+   // + setRight()
+   // + reverse()
+   // + addOnto(delta : Double)
+
+private:
+   double convertToDegrees(double r) const
+   {
+      double d = r * (180.0 / M_PI);
+      return d;
+   }
+   double convertToRadians(double d) const
+   {
+      double r = (2 * M_PI) * (d / 360);
+      return r;
+   }
+
+   /************************************
+    * ANGLE : NORMALIZE
+    ************************************/
+   double Angle::normalize(double radians) const
+   {
+      
+      return -99.9;
+   }
+};
