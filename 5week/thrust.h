0dfc1686bccaaf98f4ab5883308e2f4a691bd59b
HyrumB <hyrumbullock@gmail.com>
2024-05-22 09:38:05 -0600

    got thrust to 70%

--- 5week/thrust.h
+++ 5week/thrust.h
@@ -9,20 +9,20 @@
 
 #pragma once
 
-#include "uiInteract.h"  // for Interface
+#include "uiInteract.h" // for Interface
 
 class TestLander;
 class TestThrust;
 
- /*****************************************************
-  * THRUST
-  * Represents activation of thrusters
-  *****************************************************/
+/*****************************************************
+ * THRUST
+ * Represents activation of thrusters
+ *****************************************************/
 class Thrust
 {
    friend TestLander;
    friend TestThrust;
-   
+
 public:
    // Thrust is initially turned off
    Thrust() : mainEngine(false), clockwise(false), counterClockwise(false) {}
@@ -40,16 +40,21 @@ public:
    }
 
    // reflect what is firing
-   bool isMain()    const { return mainEngine;       }
-   bool isClock()   const { return clockwise;        }
+   bool isMain() const { return mainEngine; }
+   bool isClock() const { return clockwise; }
    bool isCounter() const { return counterClockwise; }
 
    // set the thrusters
-   void set(const Interface * pUI)
+   void set(const Interface *pUI)
    {
-      mainEngine       = true;
-      clockwise        = true;
-      counterClockwise = true;
+      if (pUI->isDown()){ mainEngine = true; }
+      else{               mainEngine = false;}
+
+      if (pUI->isLeft()) { clockwise = true; }
+      else{                clockwise = false;}
+
+      if (pUI->isRight()) { counterClockwise = true; }
+      else{                 counterClockwise = false;}
    }
 
 private:
