90020869f3362fac2cd97315051c82344c659b0a
Calvinbullock <calvinnbullock@gmail.com>
2024-05-24 13:39:31 -0600

    coast func done

--- 5week/lander.cpp
+++ 5week/lander.cpp
@@ -40,10 +40,27 @@ Acceleration Lander :: input(const Thrust& thrust, double gravity)
 /******************************************************************
  * COAST
  * What happens when we coast?
+ * newPos = currentPos + velocity * time + .5 * acceleration * time^2
  *******************************************************************/
-void Lander :: coast(Acceleration & acceleration, double time)
+void Lander ::coast(Acceleration &acceleration, double time)
 {
-   double currentPosX = pos.getX();
+   // calculate new velocity
+   velocity.add(acceleration, time);
 
-   pos.setX(currentPosX + (acceleration.getDDX() * time));
+   // get variables
+   // NOTE We decided on readability over memory efficiency
+   double currentPosX = pos.getX();
+   double currentPosY = pos.getY();
+   double ddx = acceleration.getDDX();
+   double ddy = acceleration.getDDY();
+   double dx  = velocity.getDX();
+   double dy  = velocity.getDY();
+
+   // calculate new x/y position
+   double newPosX = currentPosX + (dx * time) + (0.5 * ddx * (time * time));
+   double newPosY = currentPosY + (dy * time) + (0.5 * ddy * (time * time));
+
+   // set new position
+   this->pos.setX(newPosX);
+   this->pos.setY(newPosY);
 }
