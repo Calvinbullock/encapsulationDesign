cc3665c57bf86d40b34762d1e1287bd1f3e49883
Calvinbullock <calvinnbullock@gmail.com>
2024-05-24 19:37:22 -0600

    input in lander.cpp tests passed

--- 5week/lander.cpp
+++ 5week/lander.cpp
@@ -10,7 +10,6 @@
 #include "lander.h"
 #include "acceleration.h"
 #include <cmath>
-#include <iostream>
 
 /***************************************************************
  * RESET
@@ -35,54 +34,30 @@ void Lander ::draw(const Thrust &thrust, ogstream &gout) const
 /***************************************************************
  * INPUT
  * Accept input from the Neil Armstrong
- *  a = f / m
  ***************************************************************/
 Acceleration Lander ::input(const Thrust &thrust, double gravity)
 {
    double thr = thrust.mainEngineThrust();
-    std::cout << "th " << thr << std::endl;
    double deg = angle.getDegrees();
    double ddx;
    double ddy;
 
-   if (fuel > 0 && (thrust.isMain() == true || thrust.isClock() == true || thrust.isCounter() == true))
+   // check if rotating
+   if (fuel >= 1 && (thrust.isClock() == true || thrust.isCounter() == true))
    {
       fuel--;
       angle.add(thrust.rotation());
-
-      if (deg == 0 || deg == 180)
-      {
-         // y thrust
-         ddx = sin(angle.getRadians()) * (velocity.getDX());
-         ddy = cos(angle.getRadians()) * (thr + velocity.getDY() + gravity);
-      }
-      else if (deg > 90 || deg > 270)
-      {
-         // x thrust
-         ddx = sin(angle.getRadians()) * (thr + velocity.getDX());
-         ddy = cos(angle.getRadians()) * velocity.getDY() + gravity;
-    std::cout << ddx << std::endl;
-    std::cout << ddy << std::endl;
-      }
-      else if ((deg > 0 && deg < 90) || (deg > 180 && deg < 270))
-      {
-         // (x,y)+ || (x,y)-
-         ddx = sin(angle.getRadians()) * (thr + velocity.getDX());
-         ddy = cos(angle.getRadians()) * (thr + velocity.getDY() + gravity);
-      }
-      else if ((deg > 90 && deg < 180) || (deg > 270 && deg < 360))
-      {
-         // x+,y- || x-,y+
-         ddy = cos(angle.getRadians()) * (thr + velocity.getDY() + gravity);
-         ddx = sin(angle.getRadians()) * (thr + velocity.getDX());
-      }
    }
-   else
+
+   // use main thruster
+   if (fuel >= 10 && (thrust.isMain() == true))
    {
-      ddx = sin(angle.getRadians()) * velocity.getDX();
-      ddy = cos(angle.getRadians()) * velocity.getDY() + gravity;
+      fuel -= 10;
+      ddx = -1 * (sin(angle.getRadians()) * (thr + velocity.getDX()));
+      ddy = cos(angle.getRadians()) * (thr + velocity.getDY());
    }
 
+   ddy += gravity;
    Acceleration accel = Acceleration(ddx, ddy);
    return accel;
 }
