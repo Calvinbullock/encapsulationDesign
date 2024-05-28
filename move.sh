18cd4e88a671e110b5179233274d24917f0aec6c
Calvinbullock <calvinnbullock@gmail.com>
2024-05-28 09:30:58 -0600

    move.sh update

--- move.sh
+++ move.sh
@@ -1,5 +1,5 @@
-lastWeek="4week"
-thisWeek="5week"
+lastWeek="5week"
+thisWeek="6week"
 
 cp -i ./$lastWeek/acceleration.cpp ./$thisWeek/acceleration.cpp 
 cp ./$lastWeek/acceleration.h ./$thisWeek/acceleration.h
@@ -13,5 +13,15 @@ cp ./$lastWeek/position.h ./$thisWeek/position.h
 cp ./$lastWeek/velocity.cpp ./$thisWeek/velocity.cpp
 cp ./$lastWeek/velocity.h ./$thisWeek/velocity.h
 
+cp ./$lastWeek/lander.cpp ./$thisWeek/lander.cpp
+cp ./$lastWeek/lander.h ./$thisWeek/lander.h
+
+# header(only) files
 cp ./$lastWeek/simulator.cpp ./$thisWeek/simulator.cpp
+cp ./$lastWeek/star.h ./$thisWeek/star.h
+cp ./$lastWeek/thrust.h ./$thisWeek/thrust.h
+
+# compile files
+cp ./$lastWeek/makefile ./$thisWeek/makefile
+cp ./$lastWeek/guiRun.sh ./$thisWeek/guiRun.sh
 
