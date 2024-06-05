2ec99cea8c24ac172f8b0583027622752b2dde4b
Calvinbullock <calvinnbullock@gmail.com>
2024-06-05 09:01:58 -0600

    main update

--- 7week/main.cpp
+++ 7week/main.cpp
@@ -1,6 +1,57 @@
+/* ******************************************** 
+ * TODO  Fill in header
+ * ***************************************** */
+ 
 #include "position.h"
 
+#define GRAVITY -9.8 // m/s^2
+
+
+/* ******************************************** 
+ * TODO  fill in class header
+ * ***************************************** */
+class Angle {
+
+public:
+   Angle() {}
+
+private:
+   double angle; // degrees
+      
+};
+
+
+/* ******************************************** 
+ * TODO  fill in class header
+ * ***************************************** */
+class Acceleration {
+
+public:
+   Acceleration() {}
+
+private:
+   double accel; // degrees
+      
+};
+
+
+/* ******************************************** 
+ * TODO  fill in class header
+ * ***************************************** */
+class simulater {
+
+public:
+
+private:
+   Angle angle;
+   Acceleration accel;
+   Position pos;
+};
+
+
+/* ******************************************** 
+ * TODO  main header
+ * ***************************************** */
 int main (int argc, char *argv[]) {
-   
    return 0;
 }
