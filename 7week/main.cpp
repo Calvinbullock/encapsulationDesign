8ca1824498c55e9b133b1645b9d3fe8a7d493dbf
HyrumB <hyrumbullock@gmail.com>
2024-06-05 09:56:48 -0600

    added print in the loop

--- 7week/main.cpp
+++ 7week/main.cpp
@@ -1,38 +1,39 @@
-/* ******************************************** 
+/* ********************************************
  * TODO  Fill in header
  * ***************************************** */
- 
+
 #include "position.h"
 #include <iostream>
 using namespace std;
 
 #define GRAVITY -9.8 // m/s^2
 
-
-/* ******************************************** 
+/* ********************************************
  * TODO  fill in class header
  * ***************************************** */
-class Angle {
+class Angle
+{
 
 public:
-   Angle() {
+   Angle()
+   {
    }
 
 private:
    double angle; // degrees
-      
 };
 
-
-/* ******************************************** 
+/* ********************************************
  * TODO  fill in class header
  * ***************************************** */
-class Acceleration {
+class Acceleration
+{
 
 public:
-   Acceleration(double angle, double vel ) {
-   ddx = sin(angle) * vel; 
-   ddy = cos(angle) * vel;
+   Acceleration(double angle, double vel)
+   {
+      ddx = sin(angle) * vel;
+      ddy = cos(angle) * vel;
    }
 
 private:
@@ -40,34 +41,35 @@ private:
    double ddy;
 };
 
-
-/* ******************************************** 
+/* ********************************************
  * TODO  fill in class header
  * ***************************************** */
-class Shell {
+class Shell
+{
 
 public:
-
 private:
    Angle angle;
    Acceleration accel;
    Position pos;
 };
 
-
-/* ******************************************** 
+/* ********************************************
  * TODO  main header
  * ***************************************** */
-int main (int argc, char *argv[]) {
-   double vel = 827.0; // speed of shell when leaving barrel
+int main(int argc, char *argv[])
+{
+   double vel = 827.0;   // speed of shell when leaving barrel
    double angle = 1.309; // 75 in rad
-
-   Acceleration accel(); 
+   
+   Position pos;
+   Acceleration accel();
 
    for (double i = 0; i <= 20; i++)
    {
+      cout << "Distance: " << pos.getMetersX() << "Altitude: " << pos.getMetersY() << endl;
+
    };
-   
 
    cout << "pop" << endl;
    return 0;
