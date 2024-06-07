02ce0de3af406c03e63ce98f62f775d9cc8757ba
Calvinbullock <calvinnbullock@gmail.com>
2024-06-07 10:27:49 -0600

    checkpoint p1

--- 7week/main.cpp
+++ 7week/main.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 using namespace std;
 
-
 /* ********************************************
- * TODO  fill in class header
+ * Acceleration class
+ *    handles acceleration in the simulator
  * ***************************************** */
 class Acceleration
 {
@@ -16,7 +16,7 @@ public:
    double getDDX() const { return ddx; }
    double getDDY() const { return ddy; }
 
-   void set(double angle, double vel)
+   void set(double angle, double vel, double gravity = 0)
    {
       ddx = sin(angle) * vel;
       ddy = cos(angle) * vel;
@@ -31,10 +31,14 @@ private:
  * CHANGE IN POSITION
  *    Calculates the change in position - inertia
  * ***************************************** */
-void changeInPostion(Position *currPos, Acceleration *accel, double time)
+void changeInPostion(Position *pos, Acceleration *accel, double time)
 {
-   currPos->setMetersX((currPos->getMetersX() + accel->getDDX()) * time);
-   currPos->setMetersY((currPos->getMetersY() + accel->getDDY()) * time);
+   pos->setMetersX((pos->getMetersX() + accel->getDDX()) * time);
+   pos->setMetersY((pos->getMetersY() + accel->getDDY()) * time);
+
+   // NOTE  this is the full distance formula - breaks the ans
+   /*pos->setMetersX(pos->getMetersX() + vel * time + .5 * accel->getDDX() * (time * time));*/
+   /*pos->setMetersY(pos->getMetersY() + vel * time + .5 * accel->getDDY() * (time * time));*/
 }
 
 /* ********************************************
@@ -42,24 +46,26 @@ void changeInPostion(Position *currPos, Acceleration *accel, double time)
  * ***************************************** */
 int main(int argc, char *argv[])
 {
+   double gravity = -9.8;
 
    /*double mass = 46.7;*/
    /*double force = 46.7 * 0;*/
    /*double initialAccel = computeAcceleration(force, mass);*/
 
-   double vel = 827.0;
    Position pos;
+   double angle = 1.309; // 75 degrees
+   double vel = 827.0;
 
-   double angle = 1.309; // 75 in rad
    Acceleration accel = Acceleration();
-
    accel.set(angle, vel);
 
+   // sim loop, 20 "frames"
    for (double i = 0; i <= 20; i++)
    {
       cout << "Distance: " << pos.getMetersX() << ", Altitude: " << pos.getMetersY() << endl;
       accel.set(angle, vel);
       changeInPostion(&pos, &accel, 1);
    };
+
    return 0;
 }
