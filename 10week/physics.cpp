c7d5280962937c8bc02572b0bf884a7f31a64a17
Calvinbullock <calvinnbullock@gmail.com>
2024-07-02 13:28:44 -0600

    moved maps into interp functions

--- 10week/physics.cpp
+++ 10week/physics.cpp
@@ -9,102 +9,6 @@
 
 #include "physics.h" // for the prototypes
 
-// clang-format off
-const Mapping densityFromAltitudeMap[] = {
-   {0, 1.2250000},
-   {1000, 1.1120000},
-   {2000, 1.0070000},
-   {3000, 0.9093000},
-   {4000, 0.8194000},
-   {5000, 0.7364000},
-   {6000, 0.6601000},
-   {7000, 0.5900000},
-   {8000, 0.5258000},
-   {9000, 0.4671000},
-   {10000, 0.4135000},
-   {15000, 0.1948000},
-   {20000, 0.0889100},
-   {25000, 0.0400800},
-   {30000, 0.0184100},
-   {40000, 0.0039960},
-   {50000, 0.0010270},
-   {60000, 0.0003097},
-   {70000, 0.0000828},
-   {80000, 0.0000185},
-};
-
-const Mapping speedSoundFromAltitudeMap[] = {
-   {0, 340},
-   {1000, 336},
-   {2000, 332},
-   {3000, 328},
-   {4000, 324},
-   {5000, 320},
-   {6000, 316},
-   {7000, 312},
-   {8000, 308},
-   {9000, 303},
-   {10000, 299},
-   {15000, 295},
-   {20000, 295},
-   {25000, 295},
-   {30000, 305},
-   {40000, 324},
-   {50000, 337},
-   {60000, 319},
-   {70000, 289},
-   {80000, 269}
-};
-
-const Mapping gravityFromAltitudeMap[] = {
-   {0.0, 9.807},
-   {1000.0, 9.804},
-   {2000.0, 9.801},
-   {3000.0, 9.797},
-   {4000.0, 9.794},
-   {5000.0, 9.791},
-   {6000.0, 9.788},
-   {7000.0, 9.785},
-   {8000.0, 9.782},
-   {9000.0, 9.779},
-   {10000.0, 9.776},
-   {15000.0, 9.761},
-   {20000.0, 9.745},
-   {25000.0, 9.730},
-   {30000.0, 9.715},
-   {40000.0, 9.684},
-   {50000.0, 9.654},
-   {60000.0, 9.624},
-   {70000.0, 9.594},
-   {80000.0, 9.564},
-};
-
-const Mapping dragFromMachMap[] = {
-   {0.0  , 0.0},
-   {0.300, 0.1629},
-   {0.500, 0.1659},
-   {0.700, 0.2031},
-   {0.890, 0.2597},
-   {0.920, 0.3010},
-   {0.960, 0.3287},
-   {0.980, 0.4002},
-   {1.000, 0.4258},
-   {1.020, 0.4335},
-   {1.060, 0.4483},
-   {1.240, 0.4064},
-   {1.530, 0.3663},
-   {1.990, 0.2897},
-   {2.870, 0.2297},
-   {2.890, 0.2306},
-   {5.000, 0.2656}
-};
-// clang-format on
-
-int getArrayDensity( const Mapping array[]){
-   int arraySize = sizeof(array) / sizeof(array[0]);
-   return arraySize;
-}
-
 /*********************************************************
  * LINEAR INTERPOLATION
  * From a list of domains and ranges, linear interpolate
@@ -141,8 +45,33 @@ double linearInterpolation(const Mapping mapping[], int mapLength,
  *********************************************************/
 double gravityFromAltitude(double altitude)
 {
-   return linearInterpolation(gravityFromAltitudeMap,
-                              getArrayDensity(gravityFromAltitudeMap), altitude);
+   // clang-format off
+   const Mapping gravityFromAltitudeMap[] = {
+      {0.0, 9.807},
+      {1000.0, 9.804},
+      {2000.0, 9.801},
+      {3000.0, 9.797},
+      {4000.0, 9.794},
+      {5000.0, 9.791},
+      {6000.0, 9.788},
+      {7000.0, 9.785},
+      {8000.0, 9.782},
+      {9000.0, 9.779},
+      {10000.0, 9.776},
+      {15000.0, 9.761},
+      {20000.0, 9.745},
+      {25000.0, 9.730},
+      {30000.0, 9.715},
+      {40000.0, 9.684},
+      {50000.0, 9.654},
+      {60000.0, 9.624},
+      {70000.0, 9.594},
+      {80000.0, 9.564},
+   };
+   // clang-format on
+
+   int listSize = sizeof(gravityFromAltitudeMap) / sizeof(gravityFromAltitudeMap[0]);
+   return linearInterpolation(gravityFromAltitudeMap, listSize, altitude);
 }
 
 /*********************************************************
@@ -151,8 +80,34 @@ double gravityFromAltitude(double altitude)
  *********************************************************/
 double densityFromAltitude(double altitude)
 {
-   return linearInterpolation(densityFromAltitudeMap,
-                              getArrayDensity(densityFromAltitudeMap), altitude);
+
+   // clang-format off
+   const Mapping densityFromAltitudeMap[] = {
+      {0, 1.2250000},
+      {1000, 1.1120000},
+      {2000, 1.0070000},
+      {3000, 0.9093000},
+      {4000, 0.8194000},
+      {5000, 0.7364000},
+      {6000, 0.6601000},
+      {7000, 0.5900000},
+      {8000, 0.5258000},
+      {9000, 0.4671000},
+      {10000, 0.4135000},
+      {15000, 0.1948000},
+      {20000, 0.0889100},
+      {25000, 0.0400800},
+      {30000, 0.0184100},
+      {40000, 0.0039960},
+      {50000, 0.0010270},
+      {60000, 0.0003097},
+      {70000, 0.0000828},
+      {80000, 0.0000185},
+   };
+   // clang-format on
+
+   int listSize = sizeof(densityFromAltitudeMap) / sizeof(densityFromAltitudeMap[0]);
+   return linearInterpolation(densityFromAltitudeMap, listSize, altitude);
 }
 
 /*********************************************************
@@ -161,8 +116,33 @@ double densityFromAltitude(double altitude)
  ********************************************************/
 double speedSoundFromAltitude(double altitude)
 {
-   return linearInterpolation(speedSoundFromAltitudeMap,
-                              getArrayDensity(speedSoundFromAltitudeMap), altitude);
+   // clang-format off
+   const Mapping speedSoundFromAltitudeMap[] = {
+      {0, 340},
+      {1000, 336},
+      {2000, 332},
+      {3000, 328},
+      {4000, 324},
+      {5000, 320},
+      {6000, 316},
+      {7000, 312},
+      {8000, 308},
+      {9000, 303},
+      {10000, 299},
+      {15000, 295},
+      {20000, 295},
+      {25000, 295},
+      {30000, 305},
+      {40000, 324},
+      {50000, 337},
+      {60000, 319},
+      {70000, 289},
+      {80000, 269}
+   };
+   // clang-format on
+
+   int listSize = sizeof(speedSoundFromAltitudeMap) / sizeof(speedSoundFromAltitudeMap[0]);
+   return linearInterpolation(speedSoundFromAltitudeMap, listSize, altitude);
 }
 
 /*********************************************************
@@ -171,8 +151,30 @@ double speedSoundFromAltitude(double altitude)
  *********************************************************/
 double dragFromMach(double speedMach)
 {
-   return linearInterpolation(dragFromMachMap, getArrayDensity(dragFromMachMap),
-                              speedMach);
+   // clang-format off
+   const Mapping dragFromMachMap[] = {
+      {0.0  , 0.0},
+      {0.300, 0.1629},
+      {0.500, 0.1659},
+      {0.700, 0.2031},
+      {0.890, 0.2597},
+      {0.920, 0.3010},
+      {0.960, 0.3287},
+      {0.980, 0.4002},
+      {1.000, 0.4258},
+      {1.020, 0.4335},
+      {1.060, 0.4483},
+      {1.240, 0.4064},
+      {1.530, 0.3663},
+      {1.990, 0.2897},
+      {2.870, 0.2297},
+      {2.890, 0.2306},
+      {5.000, 0.2656}
+   };
+   // clang-format on
+   
+   int listSize = sizeof(dragFromMachMap) / sizeof(dragFromMachMap[0]);
+   return linearInterpolation(dragFromMachMap, listSize, speedMach);
 }
 
 /* ********************************************
