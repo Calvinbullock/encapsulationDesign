4608e155262b12cd99c2f5ec67c6c6564715e1b0
Calvinbullock <calvinnbullock@gmail.com>
2024-07-20 11:36:53 -0600

    cleaned up simulater class

--- 13week/simulation.cpp
+++ 13week/simulation.cpp
@@ -7,4 +7,113 @@
  *    Execute one simulation of a projectile being fired.
  ************************************************************************/
 
- #include "simulation.h"  // for SIMULATION
+#include "simulation.h" // for SIMULATION
+
+/*********************************************
+ * RESET SIM
+ * Resets the sim
+ *********************************************/
+void Simulator::resetSim()
+{
+   shell.reset();
+   simTime = 0;
+   ground.reset(howitzer.getPosition());
+
+   howitzer.generatePosition(posUpperRight);
+   howitzer.setPosition(howitzer.getPosition());
+}
+
+/*********************************************
+ * GET STAT DISPLAY
+ * Display projectile and howitzer stats
+ *********************************************/
+std::string Simulator::getStatDisplay()
+{
+   Angle elevation = howitzer.getElevation();
+   Position shellPos = shell.getPojectilePosition();
+   double shellSpeed = shell.getPojectileSpeed();
+
+   if (shell.getIsFlying())
+   {
+      return "Distance: " + truncateString(shellPos.getMetersX(), 1) +
+             " m/sec\nHeight:    " + truncateString(shellPos.getMetersY(), 1) +
+             " m/sec\nSpeed:    " + truncateString(shellSpeed, 1) + " m/sec";
+   }
+   return "Elevation: " + truncateString(elevation.getDegrees(), 1) +
+          " degrees";
+}
+
+/*********************************************
+ * FIRE PROJECTILE
+ * creates then sets initial projectile values
+ *********************************************/
+void Simulator::fireProjectile(ogstream &gout)
+{
+   Position pos = howitzer.getPosition();
+   Angle a = howitzer.getElevation();
+
+   // pos.setPixelsX(pos.getPixelsX() + 5);
+   // pos.setMetersY(pos.getMetersY() + .3);
+   for (double age = 0; age <= 3; age += .1)
+   {
+      howitzer.draw(gout, age);
+   }
+
+   shell.fire(howitzer.getElevation(), pos, howitzer.getMuzzleVelocity());
+}
+
+/*********************************************
+ * ADVANCE PROJECTILE
+ * wrapper to calc next projectile position
+ *********************************************/
+void Simulator::advanceProjectile()
+{
+   simTime += SIMTIME;
+   if (shell.getIsFlying())
+      shell.advance(simTime);
+
+   // check if shell has impacted ground
+   Position pos = shell.getPojectilePosition();
+   double groundY = ground.getElevationMeters(pos);
+   shell.checkImpact(groundY);
+}
+
+/*********************************************
+ * HOWITZER CONTROLS
+ * wrapper to calc next projectile position
+ *********************************************/
+void Simulator::howizerControls(const Interface *pUI)
+{
+   if (pUI->isRight())
+      howitzer.rotate(0.04);
+
+   if (pUI->isLeft())
+      howitzer.rotate(-0.04);
+
+   if (pUI->isDown())
+      howitzer.raise(-0.01);
+
+   if (pUI->isUp())
+      howitzer.raise(0.01);
+}
+
+/*********************************************
+ * TRUNCATE STRING
+ * converts doubles into a string and knocks
+ *    off everything after decimal precision
+ *********************************************/
+std::string Simulator::truncateString(double value, int precision)
+{
+   std::string num = std::to_string(value);
+
+   for (int i = 0; i < num.length(); i++)
+   {
+      if (num[i] == '.')
+      {
+         i += precision + 1;
+         return num.erase(i);
+      }
+   }
+
+   return num;
+}
--- 13week/simulation.h
+++ 13week/simulation.h
@@ -29,7 +29,8 @@ class Simulator
 {
 public:
    Simulator(const Position &posUpperRight)
-       : ground(), howitzer(), shell(), simTime(0.0)
+       : ground(), howitzer(), shell(), simTime(0.0),
+         posUpperRight(posUpperRight)
    {
       ground = Ground(posUpperRight);
       ground.reset(howitzer.getPosition());
@@ -44,95 +45,34 @@ public:
    void draw(ogstream &gout)
    {
       ground.draw(gout);
-      howitzer.draw(gout, 3);
       shell.draw(gout);
+      howitzer.draw(gout, 3);
    }
 
-   void reset() 
-   { 
-      shell.reset(); 
+   void resetProjectile()
+   {
+      shell.reset();
       simTime = 0;
    }
 
-   std::string getStatDisplay()
-   {
-      Angle elevation = howitzer.getElevation();
-      Position shellPos = shell.getPojectilePosition();
-      double shellSpeed = shell.getPojectileSpeed();
+   void resetSim();
 
-      if (shell.getIsFlying())
-      {
-         return "Distance: " + truncateString(shellPos.getMetersX(), 1) +
-                " m/sec\nHeight:    " +
-                truncateString(shellPos.getMetersY(), 1) +
-                " m/sec\nSpeed:    " + truncateString(shellSpeed, 1) + " m/sec";
-      }
+   std::string getStatDisplay();
 
-      return "Elevation: " + truncateString(elevation.getDegrees(), 1) +
-             " degrees";
-   }
+   void fireProjectile(ogstream &gout);
 
-   void fireProjectile(ogstream &gout)
-   {
-      Position pos = howitzer.getPosition();
-      Angle a = howitzer.getElevation();
+   void advanceProjectile();
 
-      // pos.setPixelsX(pos.getPixelsX() + 5);
-      // pos.setMetersY(pos.getMetersY() + .3);
-      for (double age = 0; age <= 3; age += .1)
-      {
-         howitzer.draw(gout, age);
-      }
+   bool isTargetHit() { return shell.checkTargetImpact(ground.getTarget()); }
 
-      shell.fire(howitzer.getElevation(), pos, howitzer.getMuzzleVelocity());
-   }
-
-   void advanceProjectile()
-   {
-      simTime += SIMTIME;
-      if (shell.getIsFlying())
-         shell.advance(simTime);
-
-      // check if shell has impacted ground
-      Position pos = shell.getPojectilePosition();
-      double groundY = ground.getElevationMeters(pos);
-      shell.checkImpact(groundY);
-   }
-
-   void howizerControls(const Interface *pUI)
-   {
-      if (pUI->isRight())
-         howitzer.rotate(0.04);
-
-      if (pUI->isLeft())
-         howitzer.rotate(-0.04);
-
-      if (pUI->isDown())
-         howitzer.raise(-0.01);
-
-      if (pUI->isUp())
-         howitzer.raise(0.01);
-   }
+   void howizerControls(const Interface *pUI);
 
    bool isProjectileFlying() { return shell.getIsFlying(); }
 
 private:
-   std::string truncateString(double value, int precision)
-   {
-      std::string num = std::to_string(value);
+   std::string truncateString(double value, int precision);
 
-      for (int i = 0; i < num.length(); i++)
-      {
-         if (num[i] == '.')
-         {
-            i += precision + 1;
-            return num.erase(i);
-         }
-      }
-
-      return num;
-   }
-
+   Position posUpperRight;
    Ground ground;
    Howitzer howitzer;
    Projectile shell;
